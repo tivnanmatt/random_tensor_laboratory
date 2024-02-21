62830bc7c6d6cd8d9579fff49768690c27faec5e
tivnanmatt <tivnanmatt@gmail.com>
2024-02-21 12:01:19 -0500

    starting out the rtl package

---
+++ rtl_package/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rtl',
+      version='0.0',
+      description='Random Tensor Laboratory',
+      url='http://github.com/tivnanmatt/random_tensor_laboratory',
+      author='Matthew Tivnan',
+      author_email='tivnanmatt@gmail.com',
+      license='MIT',
+      packages=['rtl'],
+      zip_safe=False)
